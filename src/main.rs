729bbd2c38b09293dabaeb3eb992083b3fba804e
B4tiste <batiste.laloi@gmail.com>
2025-02-11 21:41:19 +0100

    Ajout CORS

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod router;
 mod tierlists;
 
 use mongodb::{Client, Database};
+use rocket_cors::{AllowedOrigins, Cors, CorsOptions};
 use router::create_routes;
 use shuttle_runtime::{CustomError, SecretStore};
 use std::sync::Arc;
@@ -9,6 +10,21 @@ use std::sync::Arc;
 #[derive(Clone, Debug)]
 pub struct AppState {
     pub db: Database,
+}
+
+// Fonction pour configurer CORS
+fn create_cors() -> Cors {
+    CorsOptions {
+        allowed_origins: AllowedOrigins::all(), // Autoriser tous les domaines
+        allowed_methods: vec!["GET".parse().unwrap(), "POST".parse().unwrap()]
+            .into_iter()
+            .collect(),
+        allowed_headers: rocket_cors::AllowedHeaders::all(),
+        allow_credentials: true,
+        ..Default::default()
+    }
+    .to_cors()
+    .expect("Erreur lors de la configuration de CORS")
 }
 
 #[shuttle_runtime::main]
@@ -27,7 +43,7 @@ async fn main(
 
     let app_state = Arc::new(AppState { db });
 
-    let rocket = rocket::build().manage(app_state);
+    let rocket = rocket::build().manage(app_state).attach(create_cors()); // 🔥 Ajout de CORS ici
 
     let rocket = create_routes(rocket);
 
